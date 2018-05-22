16740d9a3000982077fa451143d9cf02c45e5061
Saphire321 <marline.jvr@gmail.com>
2018-05-22 21:46:55 +0200

    een label gemis :-)

--- WindowsFormsApp2/MainMenu.Designer.cs
+++ WindowsFormsApp2/MainMenu.Designer.cs
@@ -93,11 +93,11 @@
             // 
             this.lblLogin.AutoSize = true;
             this.lblLogin.BackColor = System.Drawing.Color.Transparent;
-            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLogin.Location = new System.Drawing.Point(223, 6);
             this.lblLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(74, 17);
+            this.lblLogin.Size = new System.Drawing.Size(71, 16);
             this.lblLogin.TabIndex = 4;
             this.lblLogin.Text = "Welcome: ";
             // 
