fd979d3ba12dc932c6660ced0ba70298d991061f
Saphire321 <marline.jvr@gmail.com>
2018-05-29 18:11:31 +0200

    "ProductSerialNoPK"

--- Data_Access_/Cons.cs
+++ Data_Access_/Cons.cs
@@ -27,7 +27,7 @@ namespace Data_Access_
         #region Products
 
         public const string table2 = "tblProducts",
-                            table2Id = "ProductSerialPK",
+                            table2Id = "ProductSerialNoPK",
                             table2Col1 = "ProductModel",
                             table2Col2 = "ProductDetail",
                             table2Col3 = "UnitPrice",
