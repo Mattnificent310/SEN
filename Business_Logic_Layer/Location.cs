fd9aba1a4645859f66d5b5a3f31e2e844a49b4a9
Matt Maree <matt.maree@tracker.co.za>
2018-05-29 17:16:36 +0200

    Added Stored Procedure method to Locations class for searching City & Country

--- Business_Logic_Layer/Location.cs
+++ Business_Logic_Layer/Location.cs
@@ -1,5 +1,6 @@
 ﻿using Data_Access_;
 using Data_Access_Layer;
+using DataAccessLayer;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -35,13 +36,20 @@ namespace Business_Logic_Layer
 
             foreach (DataRow item in locTbl.Rows)
             {
+                DataRow row = new StoredProcedure().GetProcs("sp_SearchLocationByID", new Dictionary<string, object>
+                {
+                    {"LocationID",(int)item[Cons.table4IdFk] }
+                }).Rows[0];
                 locs.Add(new Location(
                   (int)item[Cons.table4Id],
                        item[Cons.table4Col1].ToString().Trim(),
-                       DataHandler.Search(item[Cons.table4IdFk].ToString(), Cons.table5)[Cons.table5Col1].ToString(),
-                       DataHandler.Search(DataHandler.Search(item[Cons.table4IdFk].ToString(),
-                       Cons.table5)[Cons.table5IdFk].ToString(),
-                       Cons.table6)[Cons.table6Col1].ToString()
+                       row[Cons.table5Col1].ToString(),
+                       row[Cons.table6Col1].ToString()
+
+                       //DataHandler.Search(item[Cons.table4IdFk].ToString(), Cons.table5)[Cons.table5Col1].ToString(),
+                       //DataHandler.Search(DataHandler.Search(item[Cons.table4IdFk].ToString(),
+                       //Cons.table5)[Cons.table5IdFk].ToString(),
+                       //Cons.table6)[Cons.table6Col1].ToString()
 
                   ));
             }
