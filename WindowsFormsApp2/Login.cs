65f0078a53ea7add0b085f015241b49c198926aa
Saphire321 <marline.jvr@gmail.com>
2018-05-29 15:55:45 +0200

    exception message

--- WindowsFormsApp2/Login.cs
+++ WindowsFormsApp2/Login.cs
@@ -149,7 +149,8 @@ namespace WindowsFormsApp2
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Unauthorized Access Detected!");
+                //MessageBox.Show("Unauthorized Access Detected!");
+                MessageBox.Show(ex.ToString());
             }
         }
 
