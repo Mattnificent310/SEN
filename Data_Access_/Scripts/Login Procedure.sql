8840f35572cb87070d78a81f87d0bf59ee65fa96
Matt Maree <matt.maree@tracker.co.za>
2018-05-29 10:53:03 +0200

    All Stored Procedures fixed

--- Data_Access_/Scripts/Login Procedure.sql
+++ Data_Access_/Scripts/Login Procedure.sql
@@ -1,23 +1,5 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
-SET ANSI_NULLS ON
+USE SHSMS 
 GO
-SET QUOTED_IDENTIFIER ON
-GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
 CREATE OR ALTER PROCEDURE sp_Login 
 	-- Add the parameters for the stored procedure here
 	(@Username VARCHAR(50),
